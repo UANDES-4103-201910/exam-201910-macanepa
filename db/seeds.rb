f412fb8f73d7077526783988715680096d025698
Matias Canepa <macanepa@miuandes.cl>
2019-06-18 11:43:12 -0400

    Added Devise Support

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ Product.create!(brand:"Prepaid-2",model:"Prepaid-2",price:2600,short_description
 Product.create!(brand:"Sub-1",model:"Sub-1",price:440,short_description:"WI",long_description:"WIWI",product_type:"subscription")
 
 
-u1= User.create!(first_name:"Matias",last_name:"Canepa",email:"macanepa@miuandes.cl")
-a1=Address.create!(address_line_1:"WIWI",city:"WIWI",country:"WIWI",zip_code:"19191",user_id:u1.id)
-Order.create!(user_id:u1.id,billing_address:a1.id,shipping_address:a1.id)
-OrderProduct.create!(order_id:1,product_id:1)
+# u1= User.create!(first_name:"Matias",last_name:"Canepa",email:"macanepa@miuandes.cl")
+# a1=Address.create!(address_line_1:"WIWI",city:"WIWI",country:"WIWI",zip_code:"19191",user_id:u1.id)
+# Order.create!(user_id:u1.id,billing_address:a1.id,shipping_address:a1.id)
+# OrderProduct.create!(order_id:1,product_id:1)
